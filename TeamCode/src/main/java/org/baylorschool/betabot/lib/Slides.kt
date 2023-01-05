b2be1082d7fcea3a601cd45324cf4f2de320eced
DxrkzSerpent <greenserpent2007@gmail.com>
2023-01-05 13:21:23 -0500

    P Control slides w/ kG (gravity) + Roadrunner prep

--- TeamCode/src/main/java/org/baylorschool/betabot/lib/Slides.kt
+++ TeamCode/src/main/java/org/baylorschool/betabot/lib/Slides.kt
@@ -3,77 +3,68 @@
 package org.baylorschool.betabot.lib
 
 import com.acmerobotics.dashboard.config.Config
-import com.arcrobotics.ftclib.controller.PIDController
+import com.acmerobotics.roadrunner.control.PIDCoefficients
+import com.acmerobotics.roadrunner.control.PIDFController
 import com.outoftheboxrobotics.photoncore.PhotonCore
-import com.qualcomm.robotcore.hardware.DcMotor
-import com.qualcomm.robotcore.hardware.DcMotorEx
-import com.qualcomm.robotcore.hardware.DcMotorSimple
-import com.qualcomm.robotcore.hardware.Gamepad
-import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.*
+import org.baylorschool.betabot.lib.SlidePIDConfig.kg
 import org.baylorschool.betabot.lib.SlidePIDConfig.p
-import org.baylorschool.betabot.lib.SlidePIDConfig.i
-import org.baylorschool.betabot.lib.SlidePIDConfig.d
-import org.baylorschool.betabot.lib.SlidePIDConfig.f
-import org.baylorschool.betabot.lib.SlidePIDConfig.targetPosition
+import org.baylorschool.betabot.lib.SlidePIDConfig.targetPos
 import org.firstinspires.ftc.robotcore.external.Telemetry
-import kotlin.math.cos
 
 @Config
 object SlidePIDConfig {
-    @JvmField var p: Double = 0.0
-    @JvmField var i: Double = 0.0
-    @JvmField var d: Double = 0.0
-    @JvmField var f: Double = 0.0
+    @JvmField var p: Double = 0.001
+    @JvmField var kg: Double = 0.11
 
-    @JvmField var targetPosition = 0
+    @JvmField var targetPos: Double = 150.0
 }
 
 class Slides(hardwareMap: HardwareMap) {
     private val slideMotor1: DcMotorEx
     private val slideMotor2: DcMotorEx
-    private var controller: PIDController
-    private val ticks: Double = 537.7
-    private var slidePosition: Int = 0
-    private var liftPos: Double = 0.0
-    var pid: Double = 0.0
-    private var ff: Double = 0.0
-    private var slidePower: Double = 0.0
+    // private val slideFF: ElevatorFeedforward
+    private var slidePosition: Double = 0.0
+    private val pControl = PIDCoefficients(p)
+    private val controller1 = PIDFController(pControl)
+    private var slidePower = 0.0
+    private var offset = 0
+    /* private val minEncoder: Int = 0
+    private val maxEncoder: Int = 1100
+     */
 
     init {
         slideMotor1 = hardwareMap.get(DcMotorEx::class.java, "rLift")
         slideMotor2 = hardwareMap.get(DcMotorEx::class.java, "lLift")
-        controller = PIDController(p, i, d)
+        slidePosition = slideMotor1.currentPosition.toDouble()
+        offset = slidePosition.toInt()
         slideMotor1.direction = DcMotorSimple.Direction.FORWARD
         slideMotor2.direction = DcMotorSimple.Direction.FORWARD
-        slideMotor1.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
-        slideMotor2.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
-        slideMotor1.targetPosition = targetPosition
-        slideMotor2.targetPosition = targetPosition
+        slideMotor1.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        slideMotor2.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        targetPos = slidePosition - offset
         PhotonCore.enable()
+
     }
 
     fun telemetry(telemetry: Telemetry) {
         telemetry.addData("Slide Motors Position", slidePosition)
-        telemetry.addData("Target Position", targetPosition)
+        telemetry.addData("Target Position", targetPos)
     }
 
     fun slideLoop(gamepad: Gamepad) {
-        controller.setPID(p, i, d)
-        slidePosition = slideMotor1.currentPosition
-        liftPos = slideMotor1.currentPosition.toDouble()
+        slidePosition = slideMotor1.currentPosition.toDouble()
+        controller1.targetPosition = targetPos
+        slidePower = controller1.update(slidePosition) * kg
 
-        pid = controller.calculate(liftPos, targetPosition.toDouble())
-        ff = cos(Math.toRadians(targetPosition / ticks)) * f
-
-        slidePower = pid * ff
-
-        slideMotor1.power - slidePower
+        slideMotor1.power = slidePower
         slideMotor2.power = slidePower
-
-        if (gamepad.dpad_up) {
-            targetPosition += 1
-        } else if (gamepad.dpad_down) {
-            targetPosition -= 1
-        }
     }
 }
+
+private fun hardStops(value: Int, low: Int, high: Int): Int {
+    return if (value < low) low
+    else if (value > high) high
+    else value
+}
+
